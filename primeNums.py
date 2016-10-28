a9e0056ee65cafa75e23ca2ae9154f9692e9aa7b
awanjila <abemuchikan@gmail.com>
2016-10-28 16:20:06 +0300

    second last commit

--- primeNums.py
+++ primeNums.py
@@ -1,9 +1,13 @@
-#A function to generate prime numbersfrom 0 to n with asymptotic analysis
+#A function to generate prime numbers from 0 to n with asymptotic analysis
 def getPrimeNumbers():
-	n=int(input())
+
 	for num in range(0, n):  #checks the num from 0 to a number n
 		if num>1:
 			for i in range(2, num): #This second loop, gets all the numbers in range o to n that divides with numbers
 				if (num%i)==0:
+					break
+			else: 
+				print(num)
+
 		
 	
