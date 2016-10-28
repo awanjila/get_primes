050a96b614686f9a452fadac45cbeea452e53614
awanjila <abemuchikan@gmail.com>
2016-10-28 15:43:39 +0300

    This second loop, gets all the numbers in range o to n that divides with numbers

--- primeNums.py
+++ primeNums.py
@@ -3,7 +3,7 @@ def getPrimeNumbers():
 	n=int(input())
 	for num in range(0, n):  #checks the num from 0 to a number n
 		if num>1:
-			for i in range(2, num):
+			for i in range(2, num): #This second loop, gets all the numbers in range o to n that divides with numbers
 				if (num%i)==0:
 		
 	
