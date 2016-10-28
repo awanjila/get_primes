e83f946be299c56e539bc3dab6567b1849a02da5
awanjila <abemuchikan@gmail.com>
2016-10-28 15:40:50 +0300

    adding for loops to my code

---
+++ primeNums.py
@@ -0,0 +1,9 @@
+#A function to generate prime numbersfrom 0 to n with asymptotic analysis
+def getPrimeNumbers():
+	n=int(input())
+	for num in range(0, n):  #checks the num from 0 to a number n
+		if num>1:
+			for i in range(2, num):
+				if (num%i)==0:
+		
+	
