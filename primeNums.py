6a7505c741549287002af6b2090b6e4dc4f62604
awanjila <abemuchikan@gmail.com>
2016-11-16 00:30:36 +0300

    taking into account indentation in python

--- primeNums.py
+++ primeNums.py
@@ -1,17 +1,17 @@
 #A function to generate prime numbers from 0 to n with asymptotic analysis
 def getPrimeNumbers():
-        start=0
+	start=0
 	end=int(input())  #get this input from the keyboard
-        print("Prime numbers between",start,"and",end,"are:")
+	print("Prime numbers between",start,"and",end,"are:")
 	for num in range(start, end):  #checks the num from 0 to a number n
 		if num>1:
 			for i in range(2, num): #This second loop, gets all the numbers in range o to n that divides with numbers
 				if (num%i)==0:
 					break
 			else: 
-				print(nums)
+				print(num)
 
 
 
 
-getPrimeNumbers()
+print (getPrimeNumbers())
